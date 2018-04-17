b4de249c3fdf9aa668b4202666544b4900daf33a
gipesman <37566237+gipesman@users.noreply.github.com>
2018-04-17 17:11:57 +0700

    Add files via upload

---
+++ UJIAN.py
@@ -0,0 +1,18 @@
+import SUA.satu
+import SUA.dua
+import SUA.tiga
+import SUA.empat
+import SUA.lima
+import SUA.enam
+
+print (SUA.satu.fa())
+
+print (SUA.dua.fb())
+
+print (SUA.tiga.fc())
+
+print (SUA.empat.fd())
+
+print (SUA.lima.fe())
+
+print (SUA.enam.ff())
